8c6933b9f8cbd968e7e462c283f14881e319a4f0
isb <jaredlamont90@gmail.com>
2025-01-04 21:33:19 -0800

    Added: basic foundation for site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Photo from "./pages/Photo";
+import Film from "./pages/Film";
+import Code from "./pages/Code";
+
+import "./App.css";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("HOME");
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case "HOME":
+        return <Home />;
+      case "ABOUT":
+        return <About />;
+      case "PHOTO":
+        return <Photo />;
+      case "CODE":
+        return <Code />;
+      case "FILM":
+        return <Film />;
+      default:
+        return <Home />;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Pass state and setter function to NavBar */}
+      <NavBar currentPage={currentPage} setCurrentPage={setCurrentPage} />
+      <main style={{
+        marginTop: 40,
+        margin: 'auto',
+        textAlign: 'center',
+        display: 'flex',
+        justifyContent:'center'}}>
+        {renderPage()}
+      </main>
     </div>
   );
 }
