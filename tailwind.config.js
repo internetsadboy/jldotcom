5b35cc01ee7bce584fc75c329cf6382b25990cc2
isb <jaredlamont90@gmail.com>
2025-01-13 22:16:00 -0800

    Fixed: tailwind cdn issue and homepage config issue w nginx

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust based on your file structure
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
